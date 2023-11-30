11219ba4b92db02af611de2397e2d143a60de054
Naksh1414 <Nakshatramanglik14@gmail.com>
2023-11-30 23:23:58 +0530

    server

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ dotenv.config()
 const app = express()
 const port = process.env.PORT || 8000
 const corsOptions = {
-   origin: 'https://final-voyage.vercel.app/',
+   origin: 'https://final-voyage.vercel.app',
    credentials: true
 }
 
